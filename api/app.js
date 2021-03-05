02319898bdde888eb0f8ed4b4bc05b93817118d9
luxjoshyua <josh.e.fielding@gmail.com>
2021-03-05 15:11:52 +1100

    worked on user sign up component

--- api/app.js
+++ api/app.js
@@ -1,26 +1,26 @@
-"use strict";
+'use strict';
 
 // load modules
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 // allow cross-origin resource sharing
-const cors = require("cors");
-const { sequelize } = require("./models");
+const cors = require('cors');
+const { sequelize } = require('./models');
 
 // import the routes
-const usersRoutes = require("./routes/user");
-const coursesRoutes = require("./routes/course");
+const usersRoutes = require('./routes/user');
+const coursesRoutes = require('./routes/course');
 // const routes = require("./routes");
 
 // variable to enable global error logging
 const enableGlobalErrorLogging =
-  process.env.ENABLE_GLOBAL_ERROR_LOGGING === "true";
+  process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
 // create the Express app
 const app = express();
 
 // setup morgan which gives us http request logging
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 // setup request body json parsing
 app.use(express.json());
@@ -29,9 +29,9 @@ app.use(express.json());
 app.use(cors());
 
 // setup a friendly greeting for the root route
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.json({
-    message: "Welcome to the REST API project!",
+    message: 'Welcome to the REST API project!',
   });
 });
 
@@ -48,13 +48,13 @@ app.get("/", (req, res) => {
 })();
 
 // tell routes to use api prefix
-app.use("/api", usersRoutes);
-app.use("/api", coursesRoutes);
+app.use('/api', usersRoutes);
+app.use('/api', coursesRoutes);
 
 // send 404 if no other route matched
 app.use((req, res) => {
   res.status(404).json({
-    message: "Route Not Found",
+    message: 'Route Not Found',
   });
 });
 
@@ -71,9 +71,9 @@ app.use((err, req, res, next) => {
 });
 
 // set our port
-app.set("port", process.env.PORT || 5000);
+app.set('port', process.env.PORT || 5000);
 
 // start listening on our port
-const server = app.listen(app.get("port"), () => {
+const server = app.listen(app.get('port'), () => {
   console.log(`Express server is listening on port ${server.address().port}`);
 });
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
-const express = require("express");
+const express = require('express');
 
-const { asyncHandler } = require("../middleware/async-handler");
-const { authenticateUser } = require("../middleware/auth-user");
-const { User } = require("../models");
+const { asyncHandler } = require('../middleware/async-handler');
+const { authenticateUser } = require('../middleware/auth-user');
+const { User } = require('../models');
 
 // construct router instance
 const router = express.Router();
@@ -15,11 +15,12 @@ const router = express.Router();
  *  - returns 200 HTTP status code
  */
 router.get(
-  "/users",
+  '/users',
   authenticateUser,
   asyncHandler(async (req, res) => {
     const user = req.currentUser;
     // filter the response
+    console.log('what does user look like', user);
 
     res.status(200).json({
       firstName: user.firstName,
@@ -36,16 +37,19 @@ router.get(
  *  - returns 201 HTTP status code and no content
  */
 router.post(
-  "/users",
+  '/users',
   asyncHandler(async (req, res) => {
     try {
       // create new User
       await User.create(req.body);
-      res.status(201).location("/").end();
+      console.log(
+        `User ${req.body.firstName} ${req.body.lastName} created successfully!`
+      );
+      res.status(201).location('/').end();
     } catch (error) {
       if (
-        error.name === "SequelizeValidationError" ||
-        error.name === "SequelizeUniqueConstraintError"
+        error.name === 'SequelizeValidationError' ||
+        error.name === 'SequelizeUniqueConstraintError'
       ) {
         const errors = error.errors.map((err) => err.message);
         res.status(400).json({ errors });
